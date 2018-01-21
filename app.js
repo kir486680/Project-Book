f113195ecb058abb3a34920c9288b261d1079532
kir <kir486680@gmail.com>
2018-01-21 17:54:52 +0200

    auth

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+require('./config/passport')(passport);
+
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/' , (req , res) =>{
     res.send('It works');
 });
+
+app.use('/auth' , auth);
 
 const port = process.env.PORT || 3000;
 
