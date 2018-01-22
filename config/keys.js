b514cfbe9615178f5d930267d0c1b7b260088f9b
kir <kir486680@gmail.com>
2018-01-22 03:36:20 +0200

    Change

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://kir486680:My2ndmail@ds111138.mlab.com:11138/story',
-    googleClientId:'887926731472-4fpsfg52gj0j286op4d401thugod98jh.apps.googleusercontent.com',
-    googleClientSecret:'X5WxE2nVJs22y_hP7Nd35bVe'
+  mongoURI:'mongodb://kir486680:My2ndmail@ds111138.mlab.com:11138/story',
+  googleClientID:'887926731472-4fpsfg52gj0j286op4d401thugod98jh.apps.googleusercontent.com',
+  googleClientSecret:'X5WxE2nVJs22y_hP7Nd35bVe'
 }
